c60afd8c6a0f8a78999837b13669793ec23ce724
enar <ents100@gmail.com>
2020-12-12 16:30:01 +0200

    Updates

---
+++ precache-manifest.fad8420155e971bc870893b77b2b0303.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b30de36901ea9b31d8bb226335ad17e",
+    "url": "/chuck-norris-react/index.html"
+  },
+  {
+    "revision": "266e0961844e05826c70",
+    "url": "/chuck-norris-react/static/css/2.6798ee7b.chunk.css"
+  },
+  {
+    "revision": "1543e2b5005410b202a9",
+    "url": "/chuck-norris-react/static/css/main.cc030442.chunk.css"
+  },
+  {
+    "revision": "266e0961844e05826c70",
+    "url": "/chuck-norris-react/static/js/2.88ca0373.chunk.js"
+  },
+  {
+    "revision": "1543e2b5005410b202a9",
+    "url": "/chuck-norris-react/static/js/main.2323915d.chunk.js"
+  },
+  {
+    "revision": "914c511383939fc894a6",
+    "url": "/chuck-norris-react/static/js/runtime~main.4a7d3549.js"
+  },
+  {
+    "revision": "0f1f1528623ffb2b09e34dfbad6dd62a",
+    "url": "/chuck-norris-react/static/media/animal_category.0f1f1528.jpg"
+  },
+  {
+    "revision": "6650b1d4e17800fcb60b2d8142d625f1",
+    "url": "/chuck-norris-react/static/media/career_category.6650b1d4.jpg"
+  },
+  {
+    "revision": "5cf447e6e52200232ac769d34b40dd8d",
+    "url": "/chuck-norris-react/static/media/celebrity_category.5cf447e6.jpg"
+  },
+  {
+    "revision": "c014f241a7f08f016135cc033d41ce7b",
+    "url": "/chuck-norris-react/static/media/chuckNorrisBackground.c014f241.jpg"
+  },
+  {
+    "revision": "74076bb168febf58771887b0ef88aa3b",
+    "url": "/chuck-norris-react/static/media/dev_category.74076bb1.png"
+  },
+  {
+    "revision": "5241de8057f5ca0187543cbe39c37dc0",
+    "url": "/chuck-norris-react/static/media/explicit_category.5241de80.png"
+  },
+  {
+    "revision": "0b740597d4581ab29c9ea0b268d51fa1",
+    "url": "/chuck-norris-react/static/media/fa-brands-400.0b740597.woff"
+  },
+  {
+    "revision": "8bb5cd1314ee0197cd41b4205a8094cf",
+    "url": "/chuck-norris-react/static/media/fa-brands-400.8bb5cd13.woff2"
+  },
+  {
+    "revision": "ad59a1f47cc8272aa5b6278430cae788",
+    "url": "/chuck-norris-react/static/media/fa-brands-400.ad59a1f4.svg"
+  },
+  {
+    "revision": "b6371ab780cb19ae75e95bf6e3377675",
+    "url": "/chuck-norris-react/static/media/fa-brands-400.b6371ab7.eot"
+  },
+  {
+    "revision": "cc30aaae4102610f791e1b03a826a149",
+    "url": "/chuck-norris-react/static/media/fa-brands-400.cc30aaae.ttf"
+  },
+  {
+    "revision": "381930c78d1c9a4f1fdbaf08da5df7db",
+    "url": "/chuck-norris-react/static/media/fa-regular-400.381930c7.woff2"
+  },
+  {
+    "revision": "3f4e3ce8ee4af45d67cad1ea59066519",
+    "url": "/chuck-norris-react/static/media/fa-regular-400.3f4e3ce8.eot"
+  },
+  {
+    "revision": "792caed1fabd9a137caddf9061232f36",
+    "url": "/chuck-norris-react/static/media/fa-regular-400.792caed1.woff"
+  },
+  {
+    "revision": "a399e535b628db043cc1b46e897f701d",
+    "url": "/chuck-norris-react/static/media/fa-regular-400.a399e535.svg"
+  },
+  {
+    "revision": "f6bbc34a71ac00679b52d715bb48c0dd",
+    "url": "/chuck-norris-react/static/media/fa-regular-400.f6bbc34a.ttf"
+  },
+  {
+    "revision": "014e4d6e9e33d5afa790cfaf456ae7cc",
+    "url": "/chuck-norris-react/static/media/fa-solid-900.014e4d6e.woff"
+  },
+  {
+    "revision": "86d1226a7402549a64670fa177b821a5",
+    "url": "/chuck-norris-react/static/media/fa-solid-900.86d1226a.ttf"
+  },
+  {
+    "revision": "a640354621103a0408a398edaeca61ae",
+    "url": "/chuck-norris-react/static/media/fa-solid-900.a6403546.eot"
+  },
+  {
+    "revision": "d4349293b09f88e3ed251712522ddff9",
+    "url": "/chuck-norris-react/static/media/fa-solid-900.d4349293.woff2"
+  },
+  {
+    "revision": "df6a6b860fb693455938c42c86568451",
+    "url": "/chuck-norris-react/static/media/fa-solid-900.df6a6b86.svg"
+  },
+  {
+    "revision": "2c1da18ad8cbbc0f88e5c93f1fd712ff",
+    "url": "/chuck-norris-react/static/media/fashion_category.2c1da18a.jpg"
+  },
+  {
+    "revision": "9a4985db3bf5a929e8a7bb4ed63f41e7",
+    "url": "/chuck-norris-react/static/media/food_category.9a4985db.jpg"
+  },
+  {
+    "revision": "d95ab075b1133c54b7b42e370ae1afaa",
+    "url": "/chuck-norris-react/static/media/history_category.d95ab075.jpg"
+  },
+  {
+    "revision": "f4e3497684a8e567bb5c437f601104bb",
+    "url": "/chuck-norris-react/static/media/money_category.f4e34976.jpg"
+  },
+  {
+    "revision": "7fe2613c04568f68e37fbc2ee7533ef1",
+    "url": "/chuck-norris-react/static/media/movie_category.7fe2613c.jpg"
+  },
+  {
+    "revision": "216c85274024288bedaae1883a3c70df",
+    "url": "/chuck-norris-react/static/media/music_category.216c8527.jpg"
+  },
+  {
+    "revision": "69d0b581ee239e8f39138bbfea9cf7b3",
+    "url": "/chuck-norris-react/static/media/political_category.69d0b581.jpg"
+  },
+  {
+    "revision": "5cf6e8380f7539e6f444c36c054a6ae3",
+    "url": "/chuck-norris-react/static/media/random_category.5cf6e838.jpg"
+  },
+  {
+    "revision": "6fb9dd6d46003b02763f33df4deeb8bf",
+    "url": "/chuck-norris-react/static/media/religion_category.6fb9dd6d.jpg"
+  },
+  {
+    "revision": "0982d8e011c85d0bd3c0948ebaf526b7",
+    "url": "/chuck-norris-react/static/media/science_category.0982d8e0.jpg"
+  },
+  {
+    "revision": "b86be753ebdee931863681733383d2e8",
+    "url": "/chuck-norris-react/static/media/sport_category.b86be753.jpg"
+  },
+  {
+    "revision": "ee2c41fc164fa4be7b98fb4528c5f255",
+    "url": "/chuck-norris-react/static/media/travel_category.ee2c41fc.jpg"
+  }
+]);
